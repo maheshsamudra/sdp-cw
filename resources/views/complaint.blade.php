4b866e49c8b95edcfe3f5302dba7a42f9dd93d6d
Hazeer Zacky <contact.hazky@gmail.com>
2023-12-11 13:49:59 +0530

    Complaint Single Page setup

---
+++ resources/views/complaint.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Complaints') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <!-- {{ __("You're logged in!") }} -->
+
+                    <!-- Table Start -->
+
+
+                    <form class="max-w-sm mx-auto">
+                        <div class="mb-6">
+                            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Complaint Title</label>
+                            <input type="text" id="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Enter title" required>
+                        </div>
+                        <div class="mb-6">
+                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your country</label>
+                            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option>Wildlife Department</option>
+                                <option>Forest Department</option>
+                            </select>
+                        </div>
+                        <div class="mb-6">
+                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Complaint Status</label>
+                            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option>Active</option>
+                                <option>Pending</option>
+                                <option>Ongoing</option>
+                                <option>Solved</option>
+                            </select>
+                        </div>
+
+                        <button type="button" class="focus:outline-none text-red-800 bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
+
+                    </form>
+
+                    <!-- Table End -->
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Complaints Page Route
+Route::get('/complaint', function () {
+    return view('complaint');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
