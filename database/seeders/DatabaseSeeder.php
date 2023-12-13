a1a041968a85cdfa2321490ce556d872c96dae8f
Mahesh Samudra <maheshsamudra@gmail.com>
2023-12-13 19:54:24 +0530

    Changed seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UserSeeder::class,
             DepartmentSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
